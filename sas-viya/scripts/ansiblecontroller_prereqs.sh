9153a683c1bcfb0e8108c342008d2e92e69fc0af
Anatol OPREA <anatol.oprea@tpatrat.com>
2021-04-22 19:56:00 +0300

    Removed old and obsolete Python 2.7 dependencies and update pip3 dependencies

--- sas-viya/scripts/ansiblecontroller_prereqs.sh
+++ sas-viya/scripts/ansiblecontroller_prereqs.sh
@@ -19,8 +19,8 @@ if [ -z "$INSTALL_USER" ]; then
 fi
 
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-yum install -y python36 gcc python3-setuptools python3-devel wget automake libffi-devel python3-six openssl-devel compat-openssl10 authselect-compat ncurses-compat-libs time
-pip install --upgrade pip
+yum install -y python36 gcc python3-setuptools python3-devel python3-wheel python3-six wget automake libffi-devel  openssl-devel compat-openssl10 authselect-compat ncurses-compat-libs time
+pip3 install --upgrade pip
 
 if ! type -p ansible;  then
   # install Ansible
